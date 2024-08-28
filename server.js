9fb1f85474638410fd58dcfd7071f674d9a7b279
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-08-28 22:35:06 +0300

    create basic bootcamps resource routes

--- server.js
+++ server.js
@@ -8,6 +8,41 @@ dotenv.config({ path: './config/config.env' });
 // Create express application
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'get all bootcamps',
+  });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'get single bootcamp',
+  });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(201).json({
+    success: true,
+    message: 'Create Bootcamp',
+  });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(201).json({
+    success: true,
+    message: 'Update Bootcamp',
+  });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(204).json({
+    success: true,
+    message: 'delete Bootcamp',
+  });
+});
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 
