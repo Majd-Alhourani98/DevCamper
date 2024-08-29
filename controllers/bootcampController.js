9060eb0f642e01ff49ffd639f6142cff992941ed
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-08-29 19:42:36 +0300

    handling duplicate keys

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -4,7 +4,7 @@ const appError = require('./../utils/appError');
 // description: GET all bootcamps
 // route:       GET /api/v1/bootcamps
 // access       Public
-const getAllBootcamps = async (req, res) => {
+const getAllBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await Bootcamp.find();
     res.status(200).json({
@@ -39,7 +39,7 @@ const getSingleBootcamp = async (req, res, next) => {
 // description: Create new bootcamp
 // route:       POST /api/v1/bootcamps
 // access       Private
-const createBootcamp = async (req, res) => {
+const createBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.create(req.body);
 
@@ -55,7 +55,7 @@ const createBootcamp = async (req, res) => {
 // description: Update  bootcamp
 // route:       PUT /api/v1/bootcamps/:id
 // access       Private
-const updateBootcamp = async (req, res) => {
+const updateBootcamp = async (req, res, next) => {
   try {
     const { id } = req.params;
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
@@ -77,7 +77,7 @@ const updateBootcamp = async (req, res) => {
 // description: Delete bootcamp
 // route:       DELETE /api/v1/bootcamps/:id
 // access       Private
-const deleteBootcamp = async (req, res) => {
+const deleteBootcamp = async (req, res, next) => {
   try {
     const { id } = req.params;
     const bootcamp = await Bootcamp.findByIdAndDelete(id);
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -10,13 +10,30 @@ const errorHandler = (err, req, res, next) => {
     error = new appError(message, 404);
   }
 
-  res.status(error.statusCode || 500).json({
-    success: false,
-    error: error.message || 'Server Error',
-  });
+  // Mongoose Duplicate Key Error
+  if (err.code === 11000) {
+    const message = `Duplicate field value: ${Object.values(error.keyValue)[0]}`;
+    error = new appError(message, 400);
+  }
+
+  console.log(err);
+  if (process.env.NODE_ENV === 'development') {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message || 'Server Error',
+      error: error,
+      stack: error.stack,
+    });
+  } else {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      error: error.message || 'Server Error',
+    });
+  }
 };
 
 module.exports = errorHandler;
 
 // Mongoose Error
 // 1. CastError ==> IDs
+// 2. Duplicate keys ==> code 11000
