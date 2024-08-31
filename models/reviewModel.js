88ad38bcb83fe2a9ae52886a9b27d786de82c96f
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-08-31 21:53:02 +0300

    add review model

---
+++ models/reviewModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a title for the review'],
+  },
+
+  text: {
+    type: String,
+    required: [true, 'Please add some text'],
+  },
+
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+    required: [true, 'Please add a rating between 1 and 10'],
+  },
+
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: true,
+  },
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Review = mongoose.model('review', reviewSchema);
+
+module.exports = Review;
